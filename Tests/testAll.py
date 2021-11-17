85b4dbb6c508a3f4f6a3d09920a1e60709dc9ac5
deliamorosanu <dede.morosanu2016@gmail.com>
2021-11-17 18:49:44 +0200

    m

--- Tests/testAll.py
+++
@@ -1,9 +0,0 @@
-from Tests.testCRUD import testAdaugaApartament,testStergeCheltuiala, testModificaCheltuaiala
-from Tests.testsDomain import testApartament
-
-
-def runAllTests():
-    testApartament()
-    testAdaugaApartament()
-    testStergeCheltuiala()
-    testModificaCheltuaiala()
